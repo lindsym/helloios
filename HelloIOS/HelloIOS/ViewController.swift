d07d4ceb9ca0cd9bdf5facdfb143acbbf25dc112
lindsym <lindsym@uw.edu>
2025-04-02 13:19:11 -0700

    done

---
+++ HelloIOS/HelloIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIOS
+//
+//  Created by Lindsy M on 4/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloIOS/HelloIOSTests/HelloIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSTests.swift
+//  HelloIOSTests
+//
+//  Created by Lindsy M on 4/2/25.
+//
+
+import Testing
+@testable import HelloIOS
+
+struct HelloIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
